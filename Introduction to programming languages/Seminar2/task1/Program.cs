a5850045465107444e69cc6c07d8d3e78d24060b
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-10 13:23:48 +0300

    Добавление Introduction to programming languages

---
+++ Introduction to programming languages/Seminar2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+System.Console.WriteLine("Введите трёхзначное число: ");
+
+int treh = Convert.ToInt32(Console.ReadLine());
+
+if(treh < 100 || treh > 1000){
+    System.Console.WriteLine("Вы ввели не трёхзначное число!");
+}
+else{
+    treh = (treh/100)*10 + treh%10;
+    System.Console.WriteLine("Число без второй цифры: " + treh);
+}
