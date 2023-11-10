76a6d40d34dbc60d6ebba3e4376cc9757f6183d3
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-10 15:55:09 +0300

    На входе два числа. Кратно ли второе первому

---
+++ Introduction to programming languages/Seminar2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+//Если второе число некратно первому, то программа выводит остаток от деления.
+System.Console.WriteLine("Введите два целых числа: ");
+int perv = Convert.ToInt32(Console.ReadLine());
+int vtor = Convert.ToInt32(Console.ReadLine());
+
+if(perv%vtor == 0){
+    System.Console.WriteLine("Число " + vtor + " кратно числу " + perv +"!");
+}
+else{
+    System.Console.WriteLine("Число " + vtor + " НЕ кратно числу " + perv + "! Отстаток равен " + perv%vtor);
+}
