a8daac0623764a0bc0fe7e118693893b51634281
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-10 14:06:17 +0300

    Вторая цифра в степени третьей

---
+++ Introduction to programming languages/Seminar2/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа принимает на вход трехзначное число и возводит вторую цифру этого числа в степень третьего числа
+System.Console.WriteLine("Введите трёхзначное число: ");
+//123
+int treh = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int nov = 1;
+ while (i < treh%10){
+    nov = nov * ((treh/10)%10);
+    i++;
+ }
+System.Console.WriteLine("Вторая цифра числа в степени третьего: " + nov);
