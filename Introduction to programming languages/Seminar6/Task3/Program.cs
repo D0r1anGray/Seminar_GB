8a930ef68b6b433da7f65cc24a4c5c39a65cba44
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-01 17:19:19 +0300

    Строка латиницей. Сколько гласных в строке?

---
+++ Introduction to programming languages/Seminar6/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. Выяснить, сколько среди введённых букв гласных.
+//Пример “hello” => 2 “world” => 1
+
+System.Console.Write("Введите строку латиницей: ");
+string str = Console.ReadLine();
+
+int CountVowel(string str){
+    str = str.ToLower();
+    char[] vowels = {'a','e','i','o','u','y'};
+    int count = 0;
+    for(int i = 0;i < str.Length; i++){
+        for(int j = 0; j< vowels.Length; j++){
+            if(vowels[j] == str[i]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+Console.WriteLine(CountVowel(str));
+
