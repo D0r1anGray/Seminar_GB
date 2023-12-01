1b804d7e3e7cf03e95acb39d3c9032716ac5a3a8
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-01 15:23:50 +0300

    Задача1. Массив символов в строку +  вывод строки наоборот

---
+++ Introduction to programming languages/Seminar6/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача1. Массив символов в строку
+
+char[] array = {'D','a','n','y','a'};
+
+string Stroka(char[] chars){
+    string str = string.Empty;
+    for(int i = 0; i < chars.Length; i++){
+        //str = str + chars[(chars.Length-1) - i];
+        // str = chars[i] + str; //Варианты вывода строки наоборот
+        str = str + chars[i];
+    }
+    return str;
+}
+
+string str = Stroka(array);
+System.Console.WriteLine(str);
