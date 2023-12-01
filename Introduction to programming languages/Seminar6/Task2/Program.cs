9bcfd2ed74f02285e65dae1a3ff378e028b8ea8b
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-01 16:55:25 +0300

    Задача2. На входе string, на выходе массив char

---
+++ Introduction to programming languages/Seminar6/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран.
+//Указание: Метод строки ToCharArray() не использовать.
+//Пример: “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+string text = Console.ReadLine();
+
+char[] ArrayFromString(string str){
+    char[] chars = new char[str.Length];
+    for(int i = 0; i < str.Length; i++){
+        chars[i] = str[i];
+    }
+    return chars;
+}
+
+// void ViewArray(char[] chars){
+//     for(int i = 0; i < chars.Length; i++){
+//         System.Console.Write(chars[i]);
+//     }
+// }
+
+//char[] chars = ArrayFromString(text);
+// ViewArray(chars);
+
+System.Console.WriteLine(ArrayFromString(text));
