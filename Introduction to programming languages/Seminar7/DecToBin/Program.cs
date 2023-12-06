23f7ca64120529561f37f5daae547169f96a6367
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-06 18:52:30 +0300

    Перевод числа в двоичную систему

---
+++ Introduction to programming languages/Seminar7/DecToBin/Program.cs
@@ -0,0 +1,23 @@
+﻿// Перевести число в двочный код
+
+uint number = Convert.ToUInt32(Console.ReadLine());
+
+string BinaryView(uint source){
+    if(source == 0){
+        return string.Empty;
+    }
+
+    return $"{BinaryView(source / 2)}{source % 2}";
+}
+
+void PrintBinaryView(uint source){
+    if(source == 0){
+        return;
+    }
+    PrintBinaryView(source/2);
+    System.Console.Write(source%2);
+}
+
+// string value = BinaryView(number);
+// System.Console.WriteLine(value);
+PrintBinaryView(number);
